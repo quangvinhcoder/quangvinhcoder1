8f791a3057938d3e0df88c95bbc0142141490d09
quangvinhcoder <nguyenquangvinh13200307@gmail.com>
2026-03-28 20:31:22 +0700

    Tao trang cap nhat lien he

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -12,6 +12,12 @@ const routes = [
     name: "NotFound",
     component: () => import("@/views/NotFound.vue"),
   },
+  {
+    path: "/contact/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true,
+  },
 ];
 
 const router = createRouter({
